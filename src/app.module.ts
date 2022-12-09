cd0f61e31ab88febdc44e1562570e940d6d27473
Hunjoon Rhee <hunjoonrhee90@gmail.com>
2022-12-09 16:54:37 +0100

    Implementation of creating Rest Api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import MoviesController from './movies/movies.controller';
+import { AppController } from './app/app.controller';
+
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export default class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll() :Movie[]{
+        return this.moviesService.getAll();
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieId:number){
+        return this.moviesService.getOne(movieId);;
+    }
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    } 
+
+    @Delete('/:id')
+    remove(@Param('id') movieId:number) {
+        return this.moviesService.remove(movieId);
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId:number, @Body() updateData:UpdateMovieDto){
+        return this.moviesService.update(movieId, updateData);
+    }
+
+
+    
+}
